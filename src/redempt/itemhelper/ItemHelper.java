bd60d1ea9bf78f99a842139c7033c526bf7f9227
Redempt <instantiable@gmail.com>
2022-02-01 23:24:52 -0500

    Add support for attribute modifiers

--- src/redempt/itemhelper/ItemHelper.java
+++ src/redempt/itemhelper/ItemHelper.java
@@ -2,8 +2,12 @@ package redempt.itemhelper;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,6 +31,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class ItemHelper extends JavaPlugin implements Listener {
 	
@@ -60,9 +65,26 @@ public class ItemHelper extends JavaPlugin implements Listener {
 				.constraint(s -> Constraint.of(ChatColor.RED + "That trait cannot be removed!", (c, t) -> t instanceof MultiItemProperty));
 		ArgType<?> subType = propertyType.subType("value", (sender, prop, str) -> prop.getArgType().convert(sender, null, str))
 				.tab((sender, args, prev) -> prev.getArgType().tabComplete(sender, args, null));
+		ArgType<Attribute> attributeType = ArgType.of("attribute", Attribute.class);
+		ArgType<EquipmentSlot> slotType = ArgType.of("slot", EquipmentSlot.class);
+		ArgType<AttributeModifier> modifierType = new ArgType<>("modifier", s -> {
+			if (s.startsWith("x")) {
+				return new AttributeModifier("mod", Double.parseDouble(s.substring(1)) - 1, Operation.MULTIPLY_SCALAR_1);
+			}
+			try {
+				Operation op = s.endsWith("%") ? Operation.ADD_SCALAR : Operation.ADD_NUMBER;
+				s = s.replaceAll("%$", "");
+				double amount = Double.parseDouble(s);
+				amount *= op == Operation.ADD_SCALAR ? 0.01 : 1;
+				return new AttributeModifier("mod", amount, op);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		});
 		
 		new CommandParser(this.getResource("command.rdcml"))
-				.setArgTypes(propertyType, subType)
+				.setArgTypes(propertyType, subType, attributeType, modifierType, slotType)
 				.setContextProviders(ContextProvider.mainHand)
 				.parse().register("itemhelper", this);
 	}
@@ -80,6 +102,25 @@ public class ItemHelper extends JavaPlugin implements Listener {
 			return;
 		}
 		item = prop.remove(item, value);
+		sender.setItemInHand(item);
+	}
+	
+	@CommandHook("addattrmod")
+	public void addAttributeModifier(Player sender, Attribute attr, AttributeModifier mod, EquipmentSlot slot, ItemStack item) {
+		ItemMeta meta = item.getItemMeta();
+		if (slot != null) {
+			mod = new AttributeModifier(UUID.randomUUID(), "mod", mod.getAmount(), mod.getOperation(), slot);
+		}
+		meta.addAttributeModifier(attr, mod);
+		item.setItemMeta(meta);
+		sender.setItemInHand(item);
+	}
+	
+	@CommandHook("removeattrmods")
+	public void removeAttributeModifiers(Player sender, Attribute attr, ItemStack item) {
+		ItemMeta meta = item.getItemMeta();
+		meta.getAttributeModifiers(attr).forEach(m -> meta.removeAttributeModifier(attr, m));
+		item.setItemMeta(meta);
 		sender.setItemInHand(item);
 	}
 	
